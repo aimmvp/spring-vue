4395bf2243127653061df7cafd373791420619de
s0wnd <aimmvp@gmail.com>
2021-03-03 16:24:44 +0900

    create back-end scaffold

---
+++ src/test/java/me/s0wnd/springvue/SpringVueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.s0wnd.springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
